1971c1a18f8d0ddc7aa77c2c2f3456b6937a5f3b
ssc5 <schah063@uottawa.ca>
2015-10-14 17:55:27 -0700

    nothing much

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 
 int main () {
+	std::cout << "One more line" << std::endl;
 	std::cout << "Another change" << std::endl;	
 	std::cout << "Hello World" << std::endl;
 	return 0;
