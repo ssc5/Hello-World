861be3db4868b97e9b8a7e80d5140ad615140b7e
ssc5 <schah063@uottawa.ca>
2015-10-16 13:28:44 -0700

    does it conflict

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream> 
 
 int main () {
-	std::cout << "fun with branches" << std::endl;
+	std::cout << "does it conflict" << std::endl;
 	//coment
 	std::cout << "One more line" << std::endl;
 	std::cout << "Another change" << std::endl;	
