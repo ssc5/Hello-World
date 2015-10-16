847b4b9751d6f3cb671217ef26ef848ee9b3095a
ssc5 <schah063@uottawa.ca>
2015-10-16 13:44:49 -0700

    newline

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 
 int main () {
+	std::cout << "should be fine right" << std::endl;
 	std::cout << "diverging that history" << std::endl;
 	std::cout << "does it conflict" << std::endl;
 	//coment
