8bc837f67811e0426302460d8dcbb2f0f925f69d
ssc5 <schah063@uottawa.ca>
2015-10-16 09:54:44 -0700

    Made some changes on the new branch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 
 int main () {
+	std::cout << "fun with branches" << std::endl;
 	//coment
 	std::cout << "One more line" << std::endl;
 	std::cout << "Another change" << std::endl;	
